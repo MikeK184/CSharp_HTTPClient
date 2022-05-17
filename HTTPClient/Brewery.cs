e40e4a6de3c01d86e831a9158c15924b85d9c694
A407.09 <A407.09@A407-09>
2022-05-17 20:52:29 +0200

    Projektdateien hinzufügen.

---
+++ HTTPClient/Brewery.cs
@@ -0,0 +1,58 @@
+﻿
+
+    using System;
+    using Newtonsoft.Json;
+
+    public partial class Brewery
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("brewery_type")]
+        public string BreweryType { get; set; }
+
+        [JsonProperty("street")]
+        public object Street { get; set; }
+
+        [JsonProperty("address_2")]
+        public object Address2 { get; set; }
+
+        [JsonProperty("address_3")]
+        public object Address3 { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("county_province")]
+        public object CountyProvince { get; set; }
+
+        [JsonProperty("postal_code")]
+        public string PostalCode { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("longitude")]
+        public object Longitude { get; set; }
+
+        [JsonProperty("latitude")]
+        public object Latitude { get; set; }
+
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+
+        [JsonProperty("website_url")]
+        public object WebsiteUrl { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+    }
---
+++ HTTPClient/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+HttpClient client = new HttpClient();
+
+var message = new HttpRequestMessage();
+message.RequestUri = new Uri(@"https://api.openbrewerydb.org/breweries");
+
+var response = client.Send(message);
+response.EnsureSuccessStatusCode();
+var responseBody = response.Content.ReadAsStringAsync().Result;
+
+List <Brewery> myDeserializedClass = JsonConvert.DeserializeObject<List<Brewery>>(responseBody);
+
+var sorted = myDeserializedClass.OrderByDescending(c => c.Name);
+
+for (int i = 0; i < myDeserializedClass.Count; i++)
+{
+    //Console.WriteLine(myDeserializedClass[i].Name);
+    Console.WriteLine(sorted.ElementAt(i).Name);
+}
+
+foreach (var item in sorted)
+{
+    Console.WriteLine(item.Name);
+}
+
+Console.WriteLine(sorted);
+
